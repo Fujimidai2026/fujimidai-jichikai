508dfae4b80b552f223d10f9982d7e995c020aa3
Fujimidai2026 <tsakai0811biz@gmail.com>
2026-05-25 15:59:48 +0900

    Update popup.js

--- js/popup.js
+++ js/popup.js
@@ -1,1 +1,27 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+  const modal = document.getElementById("gomiModal");
+  const link = document.getElementById("gomiLink");
+  const closeBtn = document.querySelector(".close");
+
+  if (link) {
+    link.addEventListener("click", function(event) {
+      event.preventDefault();
+      modal.style.display = "block";
+    });
+  }
+
+  if (closeBtn) {
+    closeBtn.addEventListener("click", function() {
+      modal.style.display = "none";
+    });
+  }
+
+  window.addEventListener("click", function(event) {
+    if (event.target === modal) {
+      modal.style.display = "none";
+    }
+  });
+
+});
 
